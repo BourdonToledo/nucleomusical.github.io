367d47316fc51d363e2262719cd5ba638d2e809a
BourdonToledo <58145074+BourdonToledo@users.noreply.github.com>
2026-03-14 20:29:43 -0300

    Update Planos.jsx

--- src/Planos/Planos.jsx
+++ src/Planos/Planos.jsx
@@ -12,23 +12,31 @@ const Planos = () => {
                 <h3> Temos tres tipos de curso:</h3>
                 <div className='tabela-planos'>
 
-                        <div className='curso-nome'>
+                        <div>
                             <p> PROMOCIONAL </p>
                             <p> 4 aulas Individuais por mes </p>
                             <p> R$ 390,00 mensais </p>
                         </div>
-                        <div className='aula-mes'>
+                        <div>
                             <p> CURSO COMPLETO</p>
-                            <p> 8 aulas por mes ( 2 aulas semanais, sendo uma individual e uma coletiva ) </p>
+                            <p> 8 aulas por mes, divididas em 2 aulas semanais, sendo uma individual e uma coletiva </p>
                             <p> a partir de R$ 410 por mes </p>
                         </div>
-                        <div className='curso-preco'>
+                        <div>
                             <p> MUSICALIZAÇÃO INFANTIL </p>
                             <p> abaixo de 7 anos 4 aulas coletivas mensais </p>
                             <p> R$ 390 por mes</p>
                         </div>
-
-
+                    <div className='descontos'>
+                        <p> 20% de desconto para os melhores alunos</p>
+                        <hr />
+                        <p> 20% de desconto no 2° curso</p>
+                        <hr />
+                        <p> 20% de desconto para pessoas da mesma família</p>
+                        <hr />
+                        <p> 20% de desconto para estudantes do ensino público (1° ou 2° grau)</p>
+                    </div>
+                    
                     <p>Aceitamos dinheiro, pix e cartões de Débito ou Crédito.</p>
                 </div>
             </div>
