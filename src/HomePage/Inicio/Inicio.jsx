1b54462e4bbe09319e2b1beb8d56aa93735712e8
BourdonToledo <58145074+BourdonToledo@users.noreply.github.com>
2026-03-15 13:54:30 -0300

    Update Inicio.jsx

--- src/HomePage/Inicio/Inicio.jsx
+++ src/HomePage/Inicio/Inicio.jsx
@@ -8,11 +8,11 @@ const Inicio = () => {
     <div className='encapsula'>
 
     <div className='inicio'>
+        <img src={imagens.homeImg} alt="" />
         <div className='apresentacao'>
             <h2 className='titulo'> Aprender criando </h2>
             <p> Aprenda mais criando musicas de sua prorpia autoria, melhorando seu entendimento geral de musica</p>
         </div>
-        <img src={imagens.homeImg} alt="" />
     </div>
     </div>
   )
